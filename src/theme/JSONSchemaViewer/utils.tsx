cb284f37517215242e3942108d8609f96fbc1282
jy95 <jacques.yakoub@gmail.com>
2023-01-11 23:41:17 +0100

    chore: Start working on getQualifierMessages

--- src/theme/JSONSchemaViewer/utils.tsx
+++ src/theme/JSONSchemaViewer/utils.tsx
@@ -4,6 +4,8 @@ import React, {
   DetailsHTMLAttributes,
 } from "react"
 
+import Translate from "@docusaurus/Translate"
+
 // https://docusaurus.io/docs/swizzling#wrapping
 import TabItem from "@theme-original/TabItem"
 import Tabs from "@theme-original/Tabs"
@@ -38,7 +40,6 @@ export function Collapsible(props: {
 
 // generate a friendly name for the schema
 export function generateFriendlyName(schema: JSONSchema7Definition): string {
-  
   // unlikely at this point but technically possible
   if (typeof schema === "boolean") {
     return "boolean"
@@ -63,7 +64,13 @@ export function generateFriendlyName(schema: JSONSchema7Definition): string {
   if (schema?.type === "array") {
     // Items property give the type of the array, when present
     if (schema?.items) {
-      return `(${generateFriendlyName})[]`;
+      const linkWord = "OR"
+      const elements = (
+        Array.isArray(schema.items) ? schema.items : [schema.items]
+      ).map((subSchema) => generateFriendlyName(subSchema))
+      const uniqueItems = [...new Set(elements)]
+
+      return `(${uniqueItems.join(` ${linkWord} `)})[]`
     }
     // Otherwise keep the plain old array type
     return "array"
@@ -71,17 +78,21 @@ export function generateFriendlyName(schema: JSONSchema7Definition): string {
 
   // In "not" case, usual it is simple but I prefer to run recursively to be sure
   if (schema?.not) {
-    return `NOT (${generateFriendlyName(schema.not)})`;
+    return `NOT (${generateFriendlyName(schema.not)})`
   }
 
   // With anyOf / allOf / oneOf, we could have some circular reference(s)
   // As using @stoplight $ref resolver, we don't have to care for that (at least for now ...)
   if (schema?.anyOf || schema?.oneOf || schema?.allOf) {
-    const linkWord = (schema?.anyOf) ? "OR" : (schema?.oneOf) ? "XOR" : "AND";
-    const elements = (schema?.anyOf || schema?.oneOf || schema?.allOf as JSONSchema7Definition[]).map(subSchema => generateFriendlyName(subSchema));
-    const uniqueItems = [...new Set(elements)]; 
+    const linkWord = schema?.anyOf ? "OR" : schema?.oneOf ? "XOR" : "AND"
+    const elements = (
+      schema?.anyOf ||
+      schema?.oneOf ||
+      (schema?.allOf as JSONSchema7Definition[])
+    ).map((subSchema) => generateFriendlyName(subSchema))
+    const uniqueItems = [...new Set(elements)]
 
-    return uniqueItems.join(` ${linkWord} `);
+    return uniqueItems.join(` ${linkWord} `)
   }
 
   // When multiple types are provided, resolution becomes hard to understand
@@ -94,14 +105,156 @@ export function generateFriendlyName(schema: JSONSchema7Definition): string {
   }
 }
 
+// The heart of the plugin : Display human friendly messages
+export function getQualifierMessages(
+  schema?: JSONSchema7Definition
+): ReactNode | undefined {
+  if (schema === undefined || typeof schema === "boolean") {
+    return undefined
+  }
+
+  let result: ReactNode[] = []
+
+  // enum values
+  if (schema?.enum !== undefined) {
+    result.push(
+      <p>
+        <strong>
+          <Translate
+            values={{
+              id: "docusaurus-json-schema-viewer-plugin.schema.enumLabel",
+            }}
+          >
+            {"Possible values:"}
+          </Translate>
+        </strong>
+        {" ["}
+        {schema.enum.map((value) => (
+          <code>{value}</code>
+        ))}
+        {"]"}
+      </p>
+    )
+  }
+
+  if (result.length === 0) {
+    return undefined
+  } else {
+    return result
+  }
+}
+
+function createDetailsNode(
+  name: string,
+  schemaName: string,
+  schema: JSONSchema7,
+  required: boolean | string[]
+): JSX.Element {
+  // TODO : return create("SchemaItem", { at the top
+
+  const isRequired = Array.isArray(required)
+    ? required.includes(name)
+    : required
+
+  return (
+    <Collapsible
+      summary={
+        <div>
+          <strong>{name}</strong>
+          <span style={{ opacity: "0.6" }}>{schemaName}</span>
+          {isRequired && (
+            <strong
+              style={{
+                fontSize: "var(--ifm-code-font-size)",
+                color: "var(--ifm-color-danger)",
+              }}
+            >
+              <Translate
+                values={{
+                  id: "docusaurus-json-schema-viewer-plugin.schema.required",
+                }}
+              >
+                {"required"}
+              </Translate>
+            </strong>
+          )}
+        </div>
+      }
+      children={<div style={{ marginLeft: "1rem" }}></div>}
+    />
+  )
+}
+
 // Creates the edges or "leaves" of a schema tree. Edges can branch into sub-nodes with createDetails().
 type EdgeProps = {
   name: string
   schema: JSONSchema7Definition
+  required: string[] | boolean
 }
 
-export function createEdges({ name, schema }: EdgeProps): JSX.Element {
-  throw new Error("Function not implemented.")
+export function createEdges({
+  name,
+  schema,
+  required,
+}: EdgeProps): JSX.Element {
+  const schemaName = generateFriendlyName(schema)
+
+  if (typeof schema === "boolean") {
+    // TODO
+    return <></>
+  }
+
+  if (schema?.oneOf || schema?.anyOf) {
+    return createDetailsNode(name, schemaName, schema, required)
+  }
+
+  if (schema?.properties) {
+    return createDetailsNode(name, schemaName, schema, required)
+  }
+
+  if (schema?.additionalProperties) {
+    return createDetailsNode(name, schemaName, schema, required)
+  }
+
+  // Items can be primitive or something else more useful to display
+  let items = (
+    schema?.items !== undefined
+      ? Array.isArray(schema.items)
+        ? schema.items
+        : [schema.items]
+      : []
+  ).filter((item) => typeof item !== "boolean") as JSONSchema7[]
+
+  if (
+    items.some(
+      (item) =>
+        item.properties !== undefined ||
+        item.anyOf !== undefined ||
+        item.oneOf !== undefined
+    )
+  ) {
+    return createDetailsNode(name, schemaName, schema, required)
+  }
+
+  //primitives and array of non-objects
+
+  /*
+
+  // TODO migrate this code
+
+  // primitives and array of non-objects
+  return create("SchemaItem", {
+    collapsible: false,
+    name,
+    required: Array.isArray(required) ? required.includes(name) : required,
+    deprecated: schema.deprecated,
+    schemaDescription: schema.description,
+    schemaName: schemaName,
+    qualifierMessage: getQualifierMessage(schema),
+    defaultValue: schema.default,
+  });
+
+  */
 }
 
 // Generate properties
@@ -114,6 +267,9 @@ export function createProperties(
         return createEdges({
           name: key,
           schema: value,
+          required: Array.isArray(schema?.required)
+            ? schema.required.includes(key)
+            : false,
         })
       })}
     </>
