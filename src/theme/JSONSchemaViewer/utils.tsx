66b0347cf815b5e2750e76e862b933000ab0e68e
jy95 <jacques.yakoub@gmail.com>
2023-01-11 01:05:19 +0100

    chore: backup work (WIP)

--- src/theme/JSONSchemaViewer/utils.tsx
+++ src/theme/JSONSchemaViewer/utils.tsx
@@ -10,7 +10,10 @@ import TabItem from "@theme/TabItem"
 import type { JSONSchema7 } from "json-schema"
 
 // To keep the same type everywhere here
-type cleanSchema = Omit<JSONSchema7, "allOf">
+type CommonSchemaType = Omit<JSONSchema7, "allOf">
+
+// https://stackoverflow.com/a/69328045/6149867
+type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] }
 
 // For collapse component
 export function Collapsible(props: {
@@ -28,5 +31,22 @@ export function Collapsible(props: {
       <summary>{summary}</summary>
       {children}
     </details>
+  )
+}
+
+// To handle Schema Composition (anyOf, oneOf)
+// Remind that thanks prefiltering, we don't have to handle allOf case
+export function renderAnyOneOf(
+  schema:
+    | WithRequired<CommonSchemaType, "oneOf">
+    | WithRequired<CommonSchemaType, "anyOf">
+): JSX.Element {
+  let typeOf = schema.oneOf ? "oneOf" : "anyOf"
+
+  return (
+    <div>
+      <span className="badge badge--info">{typeOf}</span>
+      <Tabs>{/** TODO  */}</Tabs>
+    </div>
   )
 }
