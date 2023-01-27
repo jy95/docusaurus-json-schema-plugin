4c611a0936bf1dd2d865208e77ddcc4f57f18358
jy95 <jacques.yakoub@gmail.com>
2023-01-27 23:29:18 +0100

    chore: revert to previous version

--- testsite/src/pages/boxFileSchema.tsx
+++ testsite/src/pages/boxFileSchema.tsx
@@ -9,29 +9,20 @@ import JSONSchemaEditor from "@theme/JSONSchemaEditor"
 
 import Schema from "@site/static/schemas/realWorld/boxFileSchema.json"
 
-function BoxFileSchemaInner(): JSX.Element {
-  //const { colorMode } = useColorMode()
-  return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      <div style={{ boxSizing: "border-box", width: "50%" }}>
-        <JSONSchemaViewer schema={Schema} />
-      </div>
-      <div style={{ boxSizing: "border-box", width: "50%" }}>
-        <JSONSchemaEditor schema={Schema} theme={"vs-dark"} />
-      </div>
-    </div>
-  )
-}
-
-function BoxFileSchema(): JSX.Element {
+export default function BoxFileSchema(): JSX.Element {
   return (
     <Layout
       title={`Box File JSON Schema`}
       description="Description will go into a meta tag in <head />"
     >
-      <BoxFileSchemaInner />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <div style={{ boxSizing: "border-box", width: "50%" }}>
+          <JSONSchemaViewer schema={Schema} />
+        </div>
+        <div style={{ boxSizing: "border-box", width: "50%" }}>
+          <JSONSchemaEditor schema={Schema} theme={"vs-dark"} />
+        </div>
+      </div>
     </Layout>
   )
 }
-
-export default BoxFileSchema
