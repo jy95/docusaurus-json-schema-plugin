632073363b9ed99ea7ffe9fdcfff3bd3d61e405a
jy95 <jacques.yakoub@gmail.com>
2023-01-27 23:56:19 +0100

    chore: refactor page

--- testsite/src/pages/boxFileSchema.tsx
+++ testsite/src/pages/boxFileSchema.tsx
@@ -2,34 +2,27 @@ import React from "react"
 import Layout from "@theme/Layout"
 import { useColorMode } from "@docusaurus/theme-common"
 import BrowserOnly from "@docusaurus/BrowserOnly"
-// @ts-ignore
-import JSONSchemaViewer from "@theme/JSONSchemaViewer"
-// @ts-ignore
-import JSONSchemaEditor from "@theme/JSONSchemaEditor"
 
 import Schema from "@site/static/schemas/realWorld/boxFileSchema.json"
-
-function JSONSchemaEditorWrapper(): JSX.Element {
-  const { colorMode } = useColorMode()
-  return (
-    <JSONSchemaEditor
-      schema={Schema}
-      theme={colorMode === "dark" ? "vs-dark" : "vs"}
-    />
-  )
-}
 
 function BoxFileInnerSchema(): JSX.Element {
   return (
     <BrowserOnly fallback={<div>Loading...</div>}>
       {() => {
+        const { colorMode } = useColorMode()
+        const JSONSchemaViewer = require("@theme/JSONSchemaViewer").default
+        const JSONSchemaEditor = require("@theme/JSONSchemaEditor").default
+
         return (
           <div style={{ display: "flex", flexWrap: "wrap" }}>
             <div style={{ boxSizing: "border-box", width: "50%" }}>
               <JSONSchemaViewer schema={Schema} />
             </div>
             <div style={{ boxSizing: "border-box", width: "50%" }}>
-              <JSONSchemaEditorWrapper />
+              <JSONSchemaEditor
+                schema={Schema}
+                theme={colorMode === "dark" ? "vs-dark" : "vs"}
+              />
             </div>
           </div>
         )
