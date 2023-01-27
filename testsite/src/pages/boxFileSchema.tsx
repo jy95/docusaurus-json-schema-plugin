0d120f4e5fc4781159f65d9759d7975b681d11db
jy95 <jacques.yakoub@gmail.com>
2023-01-27 23:39:56 +0100

    chore:  rewrite doc page

--- testsite/src/pages/boxFileSchema.tsx
+++ testsite/src/pages/boxFileSchema.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "@theme/Layout"
-//import { useColorMode } from "@docusaurus/theme-common"
-//import BrowserOnly from "@docusaurus/BrowserOnly"
+import { useColorMode } from "@docusaurus/theme-common"
+import BrowserOnly from "@docusaurus/BrowserOnly"
 // @ts-ignore
 import JSONSchemaViewer from "@theme/JSONSchemaViewer"
 // @ts-ignore
@@ -9,20 +9,40 @@ import JSONSchemaEditor from "@theme/JSONSchemaEditor"
 
 import Schema from "@site/static/schemas/realWorld/boxFileSchema.json"
 
+function JSONSchemaEditorWrapper(): JSX.Element {
+  const { colorMode } = useColorMode()
+  return (
+    <JSONSchemaEditor
+      schema={Schema}
+      theme={colorMode === "dark" ? "vs-dark" : "vs"}
+    />
+  )
+}
+
+function BoxFileInnerSchema(): JSX.Element {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div style={{ boxSizing: "border-box", width: "50%" }}>
+        <JSONSchemaViewer schema={Schema} />
+      </div>
+      <div style={{ boxSizing: "border-box", width: "50%" }}>
+        <BrowserOnly fallback={<div>Loading...</div>}>
+          {() => {
+            return <JSONSchemaEditorWrapper />
+          }}
+        </BrowserOnly>
+      </div>
+    </div>
+  )
+}
+
 export default function BoxFileSchema(): JSX.Element {
   return (
     <Layout
       title={`Box File JSON Schema`}
       description="Description will go into a meta tag in <head />"
     >
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        <div style={{ boxSizing: "border-box", width: "50%" }}>
-          <JSONSchemaViewer schema={Schema} />
-        </div>
-        <div style={{ boxSizing: "border-box", width: "50%" }}>
-          <JSONSchemaEditor schema={Schema} theme={"vs-dark"} />
-        </div>
-      </div>
+      <BoxFileInnerSchema />
     </Layout>
   )
 }
