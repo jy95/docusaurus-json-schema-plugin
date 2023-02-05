cc7f08c7f35ae4cc92dd87fb0885ba2577bf9972
jy95 <jacques.yakoub@gmail.com>
2023-02-05 01:22:06 +0100

    perf: use generator function for QualifierMessages

--- src/theme/JSONSchemaViewer/utils/getQualifierMessages.tsx
+++ src/theme/JSONSchemaViewer/utils/getQualifierMessages.tsx
@@ -19,64 +19,101 @@ import {
 import type { JSONSchema, JSONSchemaNS } from "../types"
 
 type Props = {
-  schema?: JSONSchema
+  schema: JSONSchema
+}
+
+// Zero, One or multiple conditions can match
+function* conditionallyRenderQMs(
+  schema: JSONSchema
+): Generator<JSX.Element, void> {
+  // Fast fail over
+  if (schema === undefined || typeof schema === "boolean") {
+    return undefined
+  }
+
+  // Enum
+  if (schema?.enum !== undefined) {
+    yield <EnumQM schema={schema} />
+  }
+
+  // minLength / maxLength
+  if (schema?.minLength !== undefined || schema?.maxLength !== undefined) {
+    yield <StringLengthQM schema={schema} />
+  }
+
+  // minProperties / maxProperties
+  if (
+    schema?.minProperties !== undefined ||
+    schema?.maxProperties !== undefined
+  ) {
+    yield <ObjectPropertiesQM schema={schema} />
+  }
+
+  // No extra properties in object
+  if (schema?.additionalProperties === false) {
+    yield <NoExtraPropertiesQM />
+  }
+
+  // minItems / maxItems
+  if (schema?.minItems !== undefined || schema?.maxItems !== undefined) {
+    yield <ArrayNumberOfItemsQM schema={schema} />
+  }
+
+  // minContains / maxContains
+  if (
+    (schema as JSONSchemaNS.Array).minContains !== undefined ||
+    (schema as JSONSchemaNS.Array).maxContains !== undefined
+  ) {
+    yield <ArrayContainsNumberQM schema={schema} />
+  }
+
+  // No extra items in array
+  if (schema?.items === false || schema?.additionalItems === false) {
+    yield <NoExtraItemsQM />
+  }
+
+  // minimum / exclusiveMinimum / maximum / exclusiveMaximum
+  if (
+    schema?.minimum !== undefined ||
+    schema?.exclusiveMinimum !== undefined ||
+    schema?.maximum !== undefined ||
+    schema?.exclusiveMaximum !== undefined
+  ) {
+    yield <NumberBoundsQM schema={schema} />
+  }
+
+  // pattern
+  if (schema?.pattern !== undefined) {
+    yield <PatternQM schema={schema} />
+  }
+
+  // multipleOf
+  if (schema?.multipleOf !== undefined) {
+    yield <MultipleOfQM schema={schema} />
+  }
+
+  // uniqueItems
+  if (schema?.uniqueItems !== undefined && schema.uniqueItems === true) {
+    yield <ArrayUniqueItemsQM />
+  }
+
+  // Default value
+  if (schema?.default !== undefined) {
+    yield <DefaultValueQM schema={schema} />
+  }
+
+  // Const value
+  if (schema?.const !== undefined) {
+    yield <ConstantQM schema={schema} />
+  }
 }
 
 // The heart of the plugin : Display human friendly messages
 export default function QualifierMessages(props: Props): null | JSX.Element {
   const { schema } = props
 
-  if (schema === undefined || typeof schema === "boolean") {
-    return null
-  }
-
-  let result = [
-    // For enum case
-    schema?.enum !== undefined && <EnumQM schema={schema} />,
-    // minLength / maxLength case
-    (schema?.minLength !== undefined || schema?.maxLength !== undefined) && (
-      <StringLengthQM schema={schema} />
-    ),
-    // minProperties / maxProperties
-    (schema?.minProperties !== undefined ||
-      schema?.maxProperties !== undefined) && (
-      <ObjectPropertiesQM schema={schema} />
-    ),
-    // No extra properties in object
-    schema?.additionalProperties === false && <NoExtraPropertiesQM />,
-    // minItems / maxItems
-    (schema?.minItems !== undefined || schema?.maxItems !== undefined) && (
-      <ArrayNumberOfItemsQM schema={schema} />
-    ),
-    // minContains / maxContains
-    ((schema as JSONSchemaNS.Array).minContains !== undefined ||
-      (schema as JSONSchemaNS.Array).maxContains !== undefined) && (
-      <ArrayContainsNumberQM schema={schema} />
-    ),
-    // No extra items in array
-    (schema?.items === false || schema?.additionalItems === false) && (
-      <NoExtraItemsQM />
-    ),
-    // minimum / exclusiveMinimum / maximum / exclusiveMaximum
-    (schema?.minimum !== undefined ||
-      schema?.exclusiveMinimum !== undefined ||
-      schema?.maximum !== undefined ||
-      schema?.exclusiveMaximum !== undefined) && (
-      <NumberBoundsQM schema={schema} />
-    ),
-    // pattern
-    schema?.pattern !== undefined && <PatternQM schema={schema} />,
-    // multipleOf
-    schema?.multipleOf !== undefined && <MultipleOfQM schema={schema} />,
-    // uniqueItems
-    schema?.uniqueItems !== undefined && schema.uniqueItems === true && (
-      <ArrayUniqueItemsQM />
-    ),
-    // Default value
-    schema?.default !== undefined && <DefaultValueQM schema={schema} />,
-    // Const value
-    schema?.const !== undefined && <ConstantQM schema={schema} />,
-  ].filter((item) => item !== false) as JSX.Element[]
+  // Find out which messages will be triggered
+  let result = Array.from(conditionallyRenderQMs(schema))
 
   if (result.length === 0) {
     return null
