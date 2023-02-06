e2cc9a1f822762681782ce2a7dd5eef6ce0bf260
jy95 <jacques.yakoub@gmail.com>
2023-02-06 23:07:56 +0100

    test: add default value replacement in Translate

--- __mocks__/@docusaurus/Translate.tsx
+++ __mocks__/@docusaurus/Translate.tsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, {ReactNode} from "react"
 
 type Props = {
   [x: string]: any
   children: string
+  values?: {
+    [x: string]: ReactNode
+  }
 }
 
 export default function Translate(props: Props): JSX.Element {
-  // Return fallback message without translate that
-  return <>{props.children}</>
+  // Return fallback message with basic replacements (what matter is that)
+  
+  let replacedString = props.children;
+  if (props.values) {
+    Object
+      .entries(props.values)
+      .forEach( ([key, value]) => {
+        // Well enough for the tests context 
+        replacedString = replacedString.replace(new RegExp(`{${key}}`), value as string)
+      })
+  }
+  return <>{replacedString}</>
 }
