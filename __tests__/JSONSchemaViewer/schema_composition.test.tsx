988dfe844411f02413acc129ebe092011891e689
jy95 <jacques.yakoub@gmail.com>
2023-02-07 19:37:31 +0100

    test: add more tests

---
+++ __tests__/JSONSchemaViewer/schema_composition.test.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals"
+
+import { create, act } from "react-test-renderer"
+
+import JSONSchemaViewer from "../../src/theme/JSONSchemaViewer/index"
+
+// Type to prevent creating invalid mocks
+import type { JSONSchema } from "../../src/theme/JSONSchemaViewer/types"
+
+const testcases: [string, JSONSchema][] = [
+  [
+    "allOf",
+    {
+      allOf: [{ type: "string" }, { maxLength: 5 }],
+    },
+  ],
+  [
+    "anyOf",
+    {
+      anyOf: [
+        { type: "string", maxLength: 5 },
+        { type: "number", minimum: 0 },
+      ],
+    },
+  ],
+  [
+    "not",
+    {
+      not: { type: "string" },
+    },
+  ],
+  [
+    "oneOf",
+    {
+      oneOf: [
+        { type: "number", multipleOf: 5 },
+        { type: "number", multipleOf: 3 },
+      ],
+    },
+  ],
+]
+
+describe("JSONSchemaViewer - schema composition", () => {
+  test.each(testcases)("test %s", async (_title, fakeSchema) => {
+    // render the component
+    let root: any
+
+    await act(async () => {
+      root = create(<JSONSchemaViewer schema={fakeSchema} />)
+    })
+
+    // make assertions on root
+    expect(root?.toJSON()).toMatchSnapshot()
+  })
+})
