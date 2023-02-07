966445f3ab2e6f186de183fe7f67777cec11dd92
jy95 <jacques.yakoub@gmail.com>
2023-02-07 20:09:30 +0100

    test: add more tests

---
+++ __tests__/JSONSchemaViewer/type.test.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+
+// For typings autocomplete whatever your IDE
+import { expect, test, describe } from "@jest/globals"
+
+import { create, act } from "react-test-renderer"
+
+import JSONSchemaViewer from "../../src/theme/JSONSchemaViewer/index"
+
+// Type to prevent creating invalid mocks
+import type { JSONSchema } from "../../src/theme/JSONSchemaViewer/types"
+
+const testcases: [string, JSONSchema][] = [
+  [
+    "single type",
+    {
+      type: "number",
+    },
+  ],
+  [
+    "Multiple types",
+    {
+      type: ["number", "string"],
+    },
+  ],
+]
+
+describe("JSONSchemaViewer - type", () => {
+  test.each(testcases)("test %s", async (fakeSchema) => {
+    // render the component
+    let root: any
+
+    await act(async () => {
+      root = create(<JSONSchemaViewer schema={fakeSchema} />)
+    })
+
+    // make assertions on root
+    expect(root?.toJSON()).toMatchSnapshot()
+  })
+})
