a563805a09e8fb5c744da1c91e5940f19a069b9a
jy95 <jacques.yakoub@gmail.com>
2023-02-13 23:44:48 +0100

    fix: theme-json-schema.d.ts

--- src/theme-json-schema.d.ts
+++ src/theme-json-schema.d.ts
@@ -1,23 +1,23 @@
-/// <reference types="@docusaurus/module-type-aliases" />
-
 // Plugin config
 declare module "docusaurus-json-schema-plugin" {
   import type { Plugin } from "@docusaurus/types"
+  import type { PluginOptions } from "@src/index"
 
-  // No options for the time being
-  export default function themJSONSchema(): Plugin<undefined>
+  export default function themeJSONSchema(
+    options: PluginOptions
+  ): Plugin<undefined>
 }
 
 // Viewer component
 declare module "@theme/JSONSchemaViewer" {
-  import type { Props } from "./theme/JSONSchemaViewer/index"
+  import type { Props } from "@theme/JSONSchemaViewer/index"
 
   export default function JSONSchemaViewer(props: Props): JSX.Element
 }
 
 // Editor component
 declare module "@theme/JSONSchemaEditor" {
-  import type { Props } from "./theme/JSONSchemaEditor/index"
+  import type { Props } from "@theme/JSONSchemaEditor/index"
 
   export default function JSONSchemaEditor(props: Props): JSX.Element
 }
