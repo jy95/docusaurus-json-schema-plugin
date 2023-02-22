d18b24753df43e67e42b0bea6101dc022acea6be
jy95 <jacques.yakoub@gmail.com>
2023-02-22 23:28:05 +0100

    docs: allow playground to manipulate a JSON pointer
    One of my tools generates $ref at the root level so why not ?

--- testsite/src/pages/playground.tsx
+++ testsite/src/pages/playground.tsx
@@ -14,8 +14,12 @@ function PlaygroundInner(): JSX.Element {
     // The demo schema
     ...DefaultSchema,
   } as { [x: string]: any })
+
   // The schema user is currently writting
   let [customSchemaString, setCustomSchemaString] = React.useState("")
+
+  // If user put a root "$ref"
+  let [jsonPointer, setJsonPointer] = React.useState("")
 
   const { colorMode } = useColorMode()
 
@@ -27,6 +31,15 @@ function PlaygroundInner(): JSX.Element {
   function updateView() {
     try {
       let newSchema = JSON.parse(customSchemaString)
+      // if "$ref" is found at the root level and "jsonPointer" wasn't set, consider it as default
+      if (jsonPointer.length === 0 && newSchema["$ref"] !== undefined) {
+        setJsonPointer(newSchema["$ref"])
+      }
+      // Overwrite (if needed) root "$ref" in case the user wants to test out stuff
+      // monaco editor relies on that to provide valuable auto-complete
+      if (jsonPointer.length !== 0) {
+        newSchema["$ref"] = jsonPointer
+      }
       setUserSchema(newSchema)
     } catch (error) {
       // KIS warning
@@ -42,7 +55,24 @@ function PlaygroundInner(): JSX.Element {
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         <div style={{ boxSizing: "border-box", width: "50%" }}>
           <h1>Schema</h1>
-          <button onClick={() => updateView()}>Update Editor / Viewer</button>
+          <div>
+            <button onClick={() => updateView()}>Update Editor / Viewer</button>
+            &nbsp;
+            <label
+              htmlFor="jsonPointer"
+              title="If you want to cover only a specific path of your specs, such as '#/definitions/*'"
+            >
+              JSON Pointer :
+            </label>
+            &nbsp;
+            <input
+              type="text"
+              id="jsonPointer"
+              name="jsonPointer"
+              onChange={(e) => setJsonPointer(e.target.value)}
+              value={jsonPointer}
+            />
+          </div>
           <JSONSchemaEditor
             value={JSON.stringify(userSchema, null, "\t")}
             // For some reason, monaco editor can ignore empty schema when $schema is provided
@@ -67,7 +97,12 @@ function PlaygroundInner(): JSX.Element {
       </div>
       <div key={JSON.stringify(userSchema)}>
         <h1>JSON Schema Viewer</h1>
-        <JSONSchemaViewer schema={userSchema} />
+        <JSONSchemaViewer
+          schema={userSchema}
+          resolverOptions={{
+            jsonPointer: jsonPointer.length !== 0 ? jsonPointer : undefined,
+          }}
+        />
       </div>
     </>
   )
