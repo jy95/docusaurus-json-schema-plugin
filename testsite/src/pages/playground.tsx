9c20c3cbc19a5a7b212491124964f9fe61867d64
jy95 <jacques.yakoub@gmail.com>
2023-03-12 11:09:32 +0100

    docs: refactor Playground

--- testsite/src/pages/playground.tsx
+++ testsite/src/pages/playground.tsx
@@ -1,15 +1,31 @@
 import React from "react"
 import Layout from "@theme/Layout"
 import BrowserOnly from "@docusaurus/BrowserOnly"
+import ErrorBoundary from "@docusaurus/ErrorBoundary"
+import PlaygroundInner from "@site/src/pages/components/PlaygroundInner"
 
 function PlaygroundComponent(): JSX.Element {
+  // No SSR for the live preview
+  // See https://github.com/facebook/docusaurus/issues/5747
   return (
     <BrowserOnly fallback={<div>Loading...</div>}>
-      {() => {
-        const PlaygroundInner =
-          require("@site/src/pages/components/PlaygroundInner").default
-        return <PlaygroundInner />
-      }}
+      {() => (
+        <>
+          <ErrorBoundary
+            fallback={({ error, tryAgain }) => (
+              <div>
+                <p>
+                  Playground component crashed because of error: {error.message}
+                  .
+                </p>
+                <button onClick={tryAgain}>Try Again!</button>
+              </div>
+            )}
+          >
+            <PlaygroundInner />
+          </ErrorBoundary>
+        </>
+      )}
     </BrowserOnly>
   )
 }
