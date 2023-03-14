45f07d9965c7089095d30dc25cae8829dd9c97b8
jy95 <jacques.yakoub@gmail.com>
2023-03-14 19:48:38 +0100

    refactor: isolate PlaygroundInner call

--- testsite/src/pages/playground.tsx
+++ testsite/src/pages/playground.tsx
@@ -4,35 +4,27 @@ import BrowserOnly from "@docusaurus/BrowserOnly"
 import ErrorBoundary from "@docusaurus/ErrorBoundary"
 
 // Monaco isn't SSR friendly
-function PlaygroundInner(): JSX.Element {
-  const PlaygroundInnerComponent =
-    require("@site/src/pages/components/PlaygroundInner").default
-  return <PlaygroundInnerComponent />
-}
 
 function PlaygroundComponent(): JSX.Element {
   // No SSR for the live preview
   // See https://github.com/facebook/docusaurus/issues/5747
   return (
-    <BrowserOnly fallback={<div>Loading...</div>}>
-      {() => (
-        <>
-          <ErrorBoundary
-            fallback={({ error, tryAgain }) => (
-              <div>
-                <p>
-                  Playground component crashed because of error: {error.message}
-                  .
-                </p>
-                <button onClick={tryAgain}>Try Again!</button>
-              </div>
-            )}
-          >
-            <PlaygroundInner />
-          </ErrorBoundary>
-        </>
+    <ErrorBoundary
+      fallback={({ error, tryAgain }) => (
+        <div>
+          <p>Playground component crashed because of error: {error.message}.</p>
+          <button onClick={tryAgain}>Try Again!</button>
+        </div>
       )}
-    </BrowserOnly>
+    >
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => {
+          const PlaygroundInnerComponent =
+            require("@site/src/pages/components/PlaygroundInner").default
+          return <PlaygroundInnerComponent />
+        }}
+      </BrowserOnly>
+    </ErrorBoundary>
   )
 }
 
