b04dda627e853bb094baaa0f1ff87989cd311a19
jy95 <jacques.yakoub@gmail.com>
2023-03-21 21:49:56 +0100

    docs: illustrate a remote file reference

--- testsite/src/components/shared-libs/generateResolverOptions.tsx
+++ testsite/src/components/shared-libs/generateResolverOptions.tsx
@@ -1,12 +1,14 @@
 import LocalFileResolver from "@site/src/components/shared-libs/localFileResolver"
+import RemoteResolver from "@site/src/components/shared-libs/remoteResolver"
 
 type Params = {
   basePath?: string
   jsonPointer?: string
+  remote?: boolean
 }
 
 export default function generateResolverOptions(params: Params) {
-  const { basePath, jsonPointer } = params
+  const { basePath, jsonPointer, remote } = params
 
   let config = {}
 
@@ -14,6 +16,14 @@ export default function generateResolverOptions(params: Params) {
     config["resolvers"] = {
       file: LocalFileResolver(basePath),
     }
+  }
+
+  if (remote) {
+    if (config["resolvers"] === undefined) {
+      config["resolvers"] = {}
+    }
+    config["resolvers"]["http"] = RemoteResolver("http")
+    config["resolvers"]["https"] = RemoteResolver("https")
   }
 
   if (jsonPointer) {
