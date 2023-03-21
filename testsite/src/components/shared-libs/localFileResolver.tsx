3db8fcc0366122ecc9ec537a0c14b034f084de01
jy95 <jacques.yakoub@gmail.com>
2023-03-21 21:29:27 +0100

    docs: illustrate a local file reference

--- testsite/src/components/shared-libs/localFileResolver.tsx
+++ testsite/src/components/shared-libs/localFileResolver.tsx
@@ -1,21 +1,14 @@
 import generatePath from "@site/src/components/shared-libs/generatePath"
-import useBaseUrl from "@docusaurus/useBaseUrl"
 
-// Here a workaround for Docusaurus, use fetch as your assets are public at the end
+// Here a workaround for Docusaurus, as your assets are public at the end, require them
 export default function LocalFileResolver(basePath: string = "") {
   return {
     resolve: (ref: string) => {
       return new Promise((resolve, reject) => {
         const temp_url = generatePath(ref, basePath)
-        console.log(temp_url)
-        const url = useBaseUrl(temp_url)
-        fetch(url, {
-          headers: {
-            Accept: "application/json",
-          },
-        })
-          .then((response) => response.text())
-          .then((json) => resolve(json))
+        //import("@site/static/schemas/examples/array/additionalItems1.json")
+        import(`@site/static/${temp_url.substring(1)}`)
+          .then((result) => resolve(result.default))
           .catch((err) => reject(err))
       })
     },
