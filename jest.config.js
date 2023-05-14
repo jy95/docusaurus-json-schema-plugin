770c9118914369fb725b053da4e3e0940c95de3f
jy95 <jacques.yakoub@gmail.com>
2023-05-14 09:18:47 +0200

    ci: set up ts-jest with path aliases

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,7 @@ module.exports = {
 
     // https://huafu.github.io/ts-jest/user/config/#paths-mapping
     // If you use “baseUrl” and “paths” options in your tsconfig file, you should make sure the “moduleNameMapper” option in your Jest config is setup accordingly.
-    "^@theme/JSONSchemaViewer/(.*)$": "<rootDir>/src/theme/JSONSchemaViewer/$1",
-    "^@theme/JSONSchemaEditor/(.*)$": "<rootDir>/src/theme/JSONSchemaEditor/$1",
-    "^@theme/MonacoEditor/(.*)$": "<rootDir>/src/theme/MonacoEditor/$1",
+    "^@theme/(.*)$": "<rootDir>/src/theme/$1",
   },
   collectCoverageFrom: ["src/theme/JSONSchemaViewer/**/*.{js,ts,jsx,tsx}"],
   coveragePathIgnorePatterns: ["!*.d.ts"],
