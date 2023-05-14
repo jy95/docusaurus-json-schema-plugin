a301e21238d12c4cbd5e46ddb64f7c05591cb9c8
jy95 <jacques.yakoub@gmail.com>
2023-05-14 08:57:38 +0200

    ci: set up ts-jest with path aliases

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils")
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require("./tsconfig")
+
+const mappings = Object.assign(
+  {},
+  {
+    // To deal with annoying css imports that breaks tests
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  compilerOptions.paths
+)
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleNameMapper: {
-    // To deal with annoying css imports that breaks tests
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-    moduleNameMapper: pathsToModuleNameMapper(
-      compilerOptions.paths /*, { prefix: '<rootDir>/' } */
-    ),
-  },
+  moduleNameMapper: mappings,
   collectCoverageFrom: ["src/theme/JSONSchemaViewer/**/*.{js,ts,jsx,tsx}"],
   coveragePathIgnorePatterns: ["!*.d.ts"],
 }
