7e911beab01a7e6eb64aeb87f6ae26ef74666279
g. nicholas d'andrea <nick@gnidan.org>
2023-12-18 20:37:45 -0500

    Remove backwards compatibility hack for enums
    ... and update test snapshots

--- src/theme/JSONSchemaViewer/utils/QualifierMessages/Enum.tsx
+++ src/theme/JSONSchemaViewer/utils/QualifierMessages/Enum.tsx
@@ -3,9 +3,7 @@ import React from "react"
 import Translate from "@docusaurus/Translate"
 
 import { CreateValue } from "@theme/JSONSchemaViewer/JSONSchemaElements"
-import { useJSVOptionsContext } from "@theme/JSONSchemaViewer/contexts"
 import type { JSONSchema } from "@theme/JSONSchemaViewer/types"
-import { printSchemaType } from "@theme/JSONSchemaViewer/utils/QualifierMessages"
 
 type Props = {
   schema: Exclude<JSONSchema, true | false>
@@ -14,7 +12,6 @@ type Props = {
 // For "enum" property
 export default function EnumQualifierMessage(props: Props): JSX.Element {
   const { schema } = props
-  const { ValueComponent } = useJSVOptionsContext()
 
   const enumLabel = (
     <strong>
@@ -28,24 +25,14 @@ export default function EnumQualifierMessage(props: Props): JSX.Element {
     </strong>
   )
 
-  // check for a provided `ValueComponent` in viewerOptions for customized
-  // rendering of individual enum values.
-  //
-  // NOTE: for backwards compatibility if `ValueComponent` is not specified,
-  // this renders enumValues together in a single CodeBlock for the entire
-  // `schema.enum` array.
-  const enumValues = !ValueComponent
-    ? printSchemaType(schema.enum!)
-    : <ul>
+  return (
+    <div key={"enum"}>
+      {enumLabel}&nbsp;
+      <ul>
         {schema.enum!.map((value, index) => <li key={index}>
           <CreateValue value={value} schema={schema} />
         </li>)}
       </ul>
-
-  return (
-    <div key={"enum"}>
-      {enumLabel}&nbsp;
-      {enumValues}
     </div>
   )
 }
