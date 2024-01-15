a49dad7fd9be69e2a1797a4385249ad44f1559e2
jy95 <jacques.yakoub@gmail.com>
2024-01-16 00:08:02 +0100

    docs: fix breaking changes

--- testsite/src/pages/playground.tsx
+++ testsite/src/pages/playground.tsx
@@ -36,7 +36,7 @@ export default function Playground(): JSX.Element {
       description="Playground of docusaurus-json-schema-plugin"
     >
       <PlaygroundComponent />
-      <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} autoClose={1000} />
+      <ToastContainer position={"bottom-right"} autoClose={1000} />
     </Layout>
   )
 }
