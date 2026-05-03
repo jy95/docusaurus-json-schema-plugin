7b91ad4706162eba626993472d4eaabe243d3ccf
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-05-03 12:28:53 +0200

    Update index.tsx

--- src/theme/JSONSchemaViewer/index.tsx
+++ src/theme/JSONSchemaViewer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Resolver } from "@stoplight/json-ref-resolver"
 
 import { CreateNodes, Collapsible } from "@theme/JSONSchemaViewer/components"
